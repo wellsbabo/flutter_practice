9a4ed82d548bbb301d9a6ceb7a56f162b259d381
이정관 <wellsbabo@gravity.co.kr>
2023-11-08 09:27:01 +0900

    #3.1 완강

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -22,15 +22,42 @@ class App extends StatelessWidget {
     // MaterialApp: 구글느낌
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          elevation: 10,
-          title: Text('Hello flutter'), //순서에 의해 파라미터를 구분함
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Hey, Selena",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.8),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
