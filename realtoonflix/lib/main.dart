4209ca72a195c8af216239f9cf33b8600a6cbf31
JUNGKWAN LEE <wellsbabo@gmail.com>
2023-11-14 22:55:11 +0900

    #6.3 완강

---
+++ realtoonflix/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:realtoonflix/screens/home_screen.dart';
+import 'package:realtoonflix/services/api_service.dart';
+
+void main() {
+  ApiService().getTodaysToons();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ realtoonflix/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1, //AppBar 밑에 그림자 없애는 것
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
